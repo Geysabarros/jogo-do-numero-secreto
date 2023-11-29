e5bdc56e97e9b4ef5c682876c13c7a56d67a86ad
Geysa <geysabarros@yahoo.com.br>
2023-11-29 14:23:40 -0300

    projeto inicial

---
+++ desafios.js
@@ -0,0 +1,53 @@
+//1
+// function cumprimento() {
+//     console.log('Olá, mundo!');
+// }
+// cumprimento();
+
+
+//2
+// function exibirNome (nome) {
+//     console.log(`Olá, ${nome}!`);
+// }
+// exibirNome('Maria');
+
+//3
+// function dobro(numero) {
+//     return numero * 2;   
+// }
+// let resultadodobro = dobro(47);
+// console.log(resultadodobro);
+
+//4
+// function mediaDeTresNumeros(num1, num2, num3) {
+//     return (num1 + num2 + num3)/3;
+// }
+// let calculoDaMedia = mediaDeTresNumeros(8, 2, 2);
+// console.log(calculoDaMedia);
+
+//5
+// function encontrarNumeroMaior (num1, num2) {
+//     return num1 > num2 ? num1 : num2;
+// }
+// let maiorNumero = encontrarNumeroMaior(9, 7)
+// console.log(maiorNumero);
+
+//6
+// function multiplicacao (a) {
+//     return a * a;
+// }
+
+// let resultadoMultiplicacao = multiplicacao(3);
+// console.log(resultadoMultiplicacao);
+
+// 1
+// let listaGenerica [];
+
+// 2
+// let linguagensDeProgramação = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+// linguagensDeProgramação.push('Java', 'Ruby', 'GoLang');
+// console.log(linguagensDeProgramação);
+
+// 3
+// let nomes = ['Maria', 'Pedro', 'João'];
+// console.log(nomes[2]);
